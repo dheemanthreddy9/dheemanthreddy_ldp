5894676cf281d895c9c9fd19188a7872203a63ef
dheemanthreddy9 <nagadheemanth.annem@zemosolabs.com>
2026-08-10 15:34:21 +0530

    add standard typography variants

--- childparent-Mui/childparent-Mui/child-parent-mui/src/components/atoms/Typography/index.tsx
+++ childparent-Mui/childparent-Mui/child-parent-mui/src/components/atoms/Typography/index.tsx
@@ -2,7 +2,21 @@ import MuiTypography from "@mui/material/Typography";
 
 type TypographyProps = {
   text: string;
-  variant?: "subtitle1" | "body2";
+  variant?:
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "subtitle1"
+    | "subtitle2"
+    | "body1"
+    | "body2"
+    | "caption"
+    | "button"
+    | "overline"
+    | "inherit";
   className?: string;
 };
 
