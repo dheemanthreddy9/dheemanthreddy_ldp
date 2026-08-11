8c7baacbb6a83079c6bcea34edd18ddbce02326b
dheemanthreddy9 <nagadheemanth.annem@zemosolabs.com>
2026-08-11 11:32:51 +0530

    updated the styles

--- Statemanagement-Assigment/statemanagement/src/theme/index.ts
+++ Statemanagement-Assigment/statemanagement/src/theme/index.ts
@@ -13,40 +13,76 @@ export const colors = {
 };
 
 export const typography = {
-  fontFamily: "'Inter'",
-  h1: { fontFamily: "'Inter'" },
-  h2: { fontFamily: "'Inter'" },
-  h3: { fontFamily: "'Inter'" },
-  h4: { fontFamily: "'Inter'" },
+  fontFamily: "'Inter', sans-serif",
+
+  h1: {
+    fontFamily: "'Inter', sans-serif",
+  },
+
+  h2: {
+    fontFamily: "'Inter', sans-serif",
+  },
+
+  h3: {
+    fontFamily: "'Inter', sans-serif",
+  },
+
+  h4: {
+    fontFamily: "'Inter', sans-serif",
+  },
+
   h5: {
-    fontFamily: "'Inter'",
+    fontFamily: "'Inter', sans-serif",
     fontWeight: 500,
     fontSize: '20px',
     lineHeight: '30px',
     letterSpacing: '0%',
     verticalAlign: 'middle',
   },
-  h6: { fontFamily: "'Inter'" },
-  subtitle1: { fontFamily: "'Inter'" },
-  subtitle2: { fontFamily: "'Inter'", fontWeight: 500, fontSize: '16px', lineHeight: '24px' },
+
+  h6: {
+    fontFamily: "'Inter', sans-serif",
+  },
+
+  subtitle1: {
+    fontFamily: "'Inter', sans-serif",
+  },
+
+  subtitle2: {
+    fontFamily: "'Inter', sans-serif",
+    fontWeight: 500,
+    fontSize: '16px',
+    lineHeight: '24px',
+  },
+
   body1: {
-    fontFamily: "'Inter'",
+    fontFamily: "'Inter', sans-serif",
     fontWeight: 500,
     fontSize: '16px',
     lineHeight: '24px',
     letterSpacing: '0%',
     verticalAlign: 'middle',
   },
+
   body2: {
-    fontFamily: "'Inter'",
+    fontFamily: "'Inter', sans-serif",
     fontWeight: 400,
     fontSize: '14px',
     lineHeight: '20px',
     letterSpacing: '0%',
   },
-  caption: { fontFamily: "'Inter'" },
-  button: { fontFamily: "'Inter'" },
-  overline: { fontFamily: "'Inter'" },
+
+  caption: {
+    fontFamily: "'Inter', sans-serif",
+  },
+
+  button: {
+    fontFamily: "'Inter', sans-serif",
+  },
+
+  overline: {
+    fontFamily: "'Inter', sans-serif",
+  },
 };
 
 export const layout = {
@@ -54,17 +90,20 @@ export const layout = {
     borderRadius: '6px',
     boxShadow: '0px 4px 28px rgba(0, 0, 0, 0.05)',
   },
+
   header: {
     height: '36px',
     borderRadius: '6px',
     border: '1px solid #E2E8F0',
   },
+
   button: {
     height: '36px',
     borderRadius: '6px',
     padding: '8px 16px',
     gap: '4px',
   },
+
   badge: {
     height: '26px',
     borderRadius: '4px',
